0a7b43fcd12e222bb143eb56f5df59522ec634cc
yamin_tang <yamin_tang@kingdee.com>
2017-02-23 16:53:23 +0800

    自定义select组件

--- src/business/views/UserForm.vue
+++ src/business/views/UserForm.vue
@@ -1,23 +1,46 @@
 <template>
     <div>
-      <span class="item-label">省市区：</span>
-      <input-select></input-select>
+      <input-select :data="data"
+                    @event-custom-selected="selected"></input-select>
     </div>
 </template>
 <style scoped>
-  .item-label{
-    font-size: .12rem;
-    text-align: left;
-
-  }
 </style>
 <script>
     import InputSelect from './../../vui/Form/InputSelect.vue'
     export default{
         data(){
             return{
-                msg:'hello vue'
+              data: {
+                name: "客户等级",
+                required: true,
+                placeholder: "请选择",
+                value: "",
+                item: [
+                  {
+                    name: "所有客户",
+                    value: "all"
+                  },
+                  {
+                    name: "重要客户",
+                    value: "import"
+                  },
+                  {
+                    name: "普通客户",
+                    value: "geneal"
+                  },
+                  {
+                    name: "VIP客户",
+                    value: "VIP"
+                  }
+                ],
+              },
             }
+        },
+        methods:{
+          selected(value){
+            console.log("selected-value",value);
+          }
         },
         components:{
           InputSelect,
--- src/vui/Form/InputSelect.vue
+++ src/vui/Form/InputSelect.vue
@@ -1,17 +1,88 @@
 <template>
     <div class="input-select">
+      <span class="item-required" v-show="data.required">*</span>
+      <span class="item-label" v-text="data.name">省市区</span><span>&nbsp;</span>
       <div class="item-value" :class="{'active':isShowPanel}"  @click="toggle">
-        <span class="item-placeholder">请选择</span>
+        <span class="item-placeholder" v-if="isShowPlaceholder">请选择</span>
+        <span class="item-name" v-else v-text="checkName"></span>
         <span class="iconfont icon-drop-down" v-show="!isShowPanel"></span>
         <span class="iconfont icon-drop-up" v-show="isShowPanel"></span>
       </div>
       <div class="checkbox-panel" v-show="isShowPanel">
         <ul>
-          <li v-for="item in list">{{item.name}}</li>
+          <li v-for="item in data.item" @click="chooseItem(item)">{{item.name}}</li>
         </ul>
       </div>
     </div>
 </template>
+<script>
+  export default{
+      data(){
+          return{
+            //参数格式
+            /*data: {
+              name: "省市区",
+              required: true,
+              placeholder: "请选择",
+              value: "",
+             item: [
+                {
+                  name: "所有客户",
+                  value: "all"
+                },
+                {
+                  name: "重要客户",
+                  value: "import"
+                },
+                {
+                  name: "普通客户",
+                  value: "geneal"
+                }
+              ],
+            },*/
+            isShowPanel: false, //是否显示面板
+            isShowPlaceholder: true, //是否显示提示
+            checkName: "",
+          }
+      },
+      props:{
+        data: {
+          type: Object,
+          required: true
+        }
+      },
+      components:{
+
+      },
+      computed:{
+
+      },
+      watch:{
+        checkName(){
+
+        }
+      },
+      ready(){
+        this.checkName = this.data.value;
+
+      },
+      methods: {
+        toggle(){
+          this.isShowPanel = !this.isShowPanel;
+        },
+        chooseItem(item){
+          this.checkName = item.name;
+          this.toggle();
+          if(this.checkName){
+            this.isShowPlaceholder = false;
+          }else{
+            this.isShowPlaceholder = true;
+          }
+          this.$dispatch('event-custom-selected',item.value);
+        },
+      }
+  }
+</script>
 <style scoped>
   .input-select{
     display: inline-block;
@@ -19,22 +90,23 @@
     vertical-align: middle;
     font-size: .26rem;
   }
-  .checkbox-panel{
-    background: rgba(26, 32, 55, .69);
-    color: #fff;
+  .item-required{
+    display: inline-block;
+    vertical-align: middle;
+    color: #f4595a;
     font-size: 14px;
-    width: 100px;
-    position: absolute;
-    z-index: 100;
-    border-radius: 2px;
-    padding: 0 4px;
+    text-align: left;
   }
-  .checkbox-panel ul li{
-    height: 31px;
-    line-height: 31px;
-    border-bottom: 1px solid rgba(255, 255, 255, .3);
-    padding: 0 16px;
-    width: 100%;
+  .item-label{
+    display: inline-block;
+    font-size: .16rem;
+    text-align: left;
+    width: 60px;
+    text-align: justify;
+    text-justify: distribute-all-lines;
+    text-align-last: justify;
+    -moz-text-align-last: justify;
+    -webkit-text-align-last: justify;
   }
   .item-value{
     display: inline-block;
@@ -46,7 +118,7 @@
     min-width: 100px;
     margin-bottom: 4px;
   }
-  .item-placeholder{
+  .item-placeholder, .item-name{
     display: inline-block;
     width: 80px;
   }
@@ -56,41 +128,22 @@
   .item-value.active .icon-drop-up{
     color: #3cbaff;
   }
-</style>
-<script>
-
-  export default{
-      data(){
-          return{
-            list: [
-              {
-                name: "所有客户",
-                value: "all"
-              },
-              {
-                name: "重要客户",
-                value: "import"
-              },
-              {
-                name: "普通客户",
-                value: "geneal"
-              }
-            ],
-            isShowPanel: false, //是否显示面板
-          }
-      },
-      props:{
-        data: {
-
-        }
-      },
-      components:{
-
-      },
-      methods: {
-        toggle(){
-          this.isShowPanel = !this.isShowPanel;
-        }
-      }
+  .checkbox-panel{
+    background: rgba(26, 32, 55, .69);
+    color: #fff;
+    font-size: 14px;
+    width: 100px;
+    position: absolute;
+    z-index: 100;
+    border-radius: 2px;
+    padding: 0 4px;
+    margin-left: 75px;
   }
-</script>
+  .checkbox-panel ul li{
+    height: 31px;
+    line-height: 31px;
+    border-bottom: 1px solid rgba(255, 255, 255, .3);
+    padding: 0 16px;
+    width: 100%;
+  }
+</style>
